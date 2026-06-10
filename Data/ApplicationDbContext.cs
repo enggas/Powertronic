bcb89a5ab4816d2f3f1daa08c8d6c7308eb4bc0f
Engel Ariel Vargas Sánchez <engesanzincodj@gmail.com>
2026-06-09 23:12:03 -0600

    Modificacion ApplicationDbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Powertronic.Models;
 
 namespace Powertronic.Data
 {
@@ -11,5 +12,20 @@ namespace Powertronic.Data
 
 
         }
+
+        public DbSet<Adquisicion> Adquisicion { get; set; }
+        public DbSet<Cargo> Cargo { get; set; }
+        public DbSet<CategoriaProducto> CategoriaProducto { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Despacho> Despacho { get; set; }
+        public DbSet<Det_Venta> Det_Venta{ get; set; }
+        public DbSet<Detalle_Adquisicion> Detalle_Adquisicion { get; set; }
+        public DbSet<Empleado> Empleado { get; set; }
+        public DbSet<Orden_Reparacion> Orden_Reparacion { get; set; }
+        public DbSet<Producto> Producto { get; set; }
+        public DbSet<Proveedores> Proveedores { get; set; }
+        public DbSet<Venta_Prod> Venta_Prod { get; set; }
+
+
     }
 }
